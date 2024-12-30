f11a71122046175f7d94f23a8e463153c9c0b37a
Coders Uz <ismoilbek20050421@gmail.com>
2024-12-30 20:47:27 +0500

    feat: codewars Super Duper Easy problem solving

---
+++ 8-kyu/Super Duper Easy/index.js
@@ -0,0 +1,12 @@
+const problem = (x) => (typeof x === "string" ? "Error" : x * 50);
+
+problem(1);
+problem(5);
+problem(0);
+problem(3);
+problem(-3);
+problem("");
+problem(1.2);
+problem(0.03);
+problem("hello");
+problem("RyanIsCool");
