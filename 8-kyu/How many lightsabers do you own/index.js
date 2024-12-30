4418d1816ba5edc5b310181e85ade31521dfa591
Coders Uz <ismoilbek20050421@gmail.com>
2024-12-31 01:56:33 +0500

    feat: codewars How many lightsabers do you own? problem solving

---
+++ 8-kyu/How many lightsabers do you own/index.js
@@ -0,0 +1,5 @@
+const howManyLightsabersDoYouOwn = (name) => name === "Zach" ? 18 : name === undefined ? 0 : 0;
+
+howManyLightsabersDoYouOwn();
+howManyLightsabersDoYouOwn("Adam");
+howManyLightsabersDoYouOwn("Zach");
