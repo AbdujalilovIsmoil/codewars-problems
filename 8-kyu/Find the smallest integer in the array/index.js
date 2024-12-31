bb0eef3f34e59b18fd1a7b37e1a7fe9b049297c9
Coders Uz <ismoilbek20050421@gmail.com>
2024-12-31 16:54:36 +0500

    feat: codewars Find the smallest integer in the array problem solving

---
+++ 8-kyu/Find the smallest integer in the array/index.js
@@ -0,0 +1,7 @@
+const findSmallestInt = (arr) => Math.min(...arr);
+
+findSmallestInt([1, 56, 232, 12, 8]);
+findSmallestInt([78, 56, 232, 12, 8]);
+findSmallestInt([78, 56, 232, 12, 0]);
+findSmallestInt([78, 56, 232, 12, 18]);
+findSmallestInt([78, 56, 232, 412, 228]);
