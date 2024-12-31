ce2367944adeb586c37cf6ae03a5965992276a4c
Coders Uz <ismoilbek20050421@gmail.com>
2024-12-31 16:59:58 +0500

    feat: codewars Remove First and Last Character problem solving

---
+++ 8-kyu/Remove First and Last Character/index.js
@@ -0,0 +1,7 @@
+const removeChar = (str) => str.slice(1, str.length - 1) + str.slice(str.length, 1);
+
+removeChar("place");
+removeChar("person");
+removeChar("country");
+removeChar("ooopsss");
+removeChar("eloquent");
