3d5b9ca65dd6dc87af46ff401a8cdf79cc0750c0
Coders Uz <ismoilbek20050421@gmail.com>
2024-12-31 16:49:32 +0500

    feat: codewars Remove String Spaces problem solving

---
+++ 8-kyu/Remove String Spaces/index.js
@@ -0,0 +1,5 @@
+const noSpace = (x) => x.split("").filter(el => el !== " ").join("");
+
+noSpace("8aaaaa dddd r     ");
+noSpace("8 j 8   mBliB8g  imjB8B8  jl  B"),
+noSpace("8 8 Bi fk8h B 8 BB8B B B  B888 c hl8 BhB fd");
