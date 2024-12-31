f61ce55ff3f696a8f14a95c2dcbbe07625626ac6
Coders Uz <ismoilbek20050421@gmail.com>
2024-12-31 13:14:33 +0500

    feat: codewars get character from ASCII Value problem solving

---
+++ 8-kyu/get character from ASCII Value/index.js
@@ -0,0 +1,13 @@
+const getChar = (c) => String.fromCharCode(c);
+
+getChar(55);
+getChar(56);
+getChar(57);
+getChar(58);
+getChar(59);
+getChar(60);
+getChar(61);
+getChar(62);
+getChar(63);
+getChar(64);
+getChar(65);
